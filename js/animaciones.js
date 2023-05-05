efba9b20a0ba29135f2550999823d05ae2e2eb8b
EdissonAcero <109987805+EdissonFrancisco@users.noreply.github.com>
2023-05-05 16:07:06 -0500

    Update animaciones.js
    animaciones mejor control y uso, mas mantenible

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,3 +1,15 @@
+//menu hamburguesa para dispositivos moviles
+const navbarlist = document.querySelector('.navbar-list');
+const navbarcell = document.querySelector(".nav-bars");
+const hamburguer = navbarcell.querySelector("i");
+
+navbarcell.addEventListener("click", function() {
+  hamburguer.classList.toggle("fa-bars");
+  hamburguer.classList.toggle("fa-xmark");
+  navbarlist.classList.toggle('mobile-menu');
+});
+
+//contraer y mostrar el menu para monitores
 //colapsar y mostrar el navBar, mover el contenedor
 const navbar = document.querySelector('.navbar');
 const containers = document.querySelectorAll('.container');
@@ -5,41 +17,12 @@ const containers = document.querySelectorAll('.container');
 navbar.addEventListener('mouseenter', function() {
   navbar.classList.remove('collapsed');
   containers.forEach(container => {
-    container.setAttribute("style", "left:20rem");
+    container.setAttribute("style", "left:17rem");
   });
 });
 navbar.addEventListener('mouseleave', function() {
   navbar.classList.add('collapsed');
   containers.forEach(container => {
-    container.setAttribute("style", "left:7rem");
+    container.setAttribute("style", "left:6.5rem");
   });
 });
-
-// clase active 
-const item_actived = document.querySelectorAll('.navbar_list');
-
-function activarLink(event) {
-  event.preventDefault();
-  item_actived.forEach((item) => {
-    item.classList.remove('active');
-  });
-  event.currentTarget.closest('li').classList.add('active');
-}
-
-item_actived.forEach((item) => item.addEventListener('click', activarLink));
-
-/*animacion de escritura titulo*/
-const textselected = document.querySelector('.principal_home_title');
-const titletext = textselected.textContent;
-textselected.textContent = "";
-
-var i = 0;
-function writeTitle() {
-  if (i < titletext.length) {
-    textselected.textContent += titletext.charAt(i);
-    i++;
-    setTimeout(writeTitle, 10);
-  }
-}
-
-setTimeout(writeTitle, 100);
